9cc19f872a37957e071baee79fe4b2fbdea8775c
lilamargo <liliana.proy@gmail.com>
2021-12-06 21:42:06 -0600

    Final Vol1

--- PI-Pokemon-main/api/tests/routes/pokemon.spec.js
+++ PI-Pokemon-main/api/tests/routes/pokemon.spec.js
@@ -1,24 +1,38 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
-const session = require('supertest-session');
-const app = require('../../src/app.js');
-const { Pokemon, conn } = require('../../src/db.js');
+const { expect } = require("chai");
+const session = require("supertest-session");
+const app = require("../../src/app.js");
+const { Pokemon, conn } = require("../../src/db.js");
 
 const agent = session(app);
 const pokemon = {
-  name: 'Pikachu',
+  name: "Toco",
+  hp: 35,
+  attack: 55,
+  defense: 40,
+  speed: 90,
+  height: 4,
+  weight: 60,
 };
 
-describe('Pokemon routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Pokemon.sync({ force: true })
-    .then(() => Pokemon.create(pokemon)));
-  describe('GET /pokemons', () => {
-    it('should get 200', () =>
-      agent.get('/pokemons').expect(200)
-    );
+describe("Pokemon routes", () => {
+  before(() =>
+    conn.authenticate().catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    })
+  );
+  beforeEach(() =>
+    Pokemon.sync({ force: true }).then(() => Pokemon.create(pokemon))
+  );
+
+  describe("GET /pokemon/:id", () => {
+    it("should get 200", function () {
+      agent.get("/api/pokemon/25").then(() => done(expect(200)));
+    });
+  });
+  describe("GET /pokemon?name", () => {
+    it("Should be 200 un successful post", function () {
+      agent.get("/api/pokemon?name=pikachu").then(() => done(expect(200)));
+    });
   });
 });
