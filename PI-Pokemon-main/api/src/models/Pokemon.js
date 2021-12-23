2008fe2f8abbf74d69f02fd240eb131c9e34c286
lilamargo <liliana.proy@gmail.com>
2021-12-23 00:23:28 -0600

    Deploy final 5

--- PI-Pokemon-main/api/src/models/Pokemon.js
+++ PI-Pokemon-main/api/src/models/Pokemon.js
@@ -8,7 +8,7 @@ module.exports = (sequelize) => {
       type: DataTypes.UUID, // UUID: GENERA UN NUMERO RANDOM CON LETRAS Y NUMEROS PARA QUE NO SE PISE CON EL ID DE LA API POKEMON.
       defaultValue: DataTypes.UUIDV4,
       allowNull: false, //SE SETEA EN FALSO PARA QUE NO SE PERMITA QUE ESTA VACIO, O SEA ES UN CAMPO OBLIGATORIO REQUERIDO.
-      primaryKey: true, //DA LA PROPIEDAD A ID DE QUE ES LA CLAVE PRIMARIA.
+      primaryKey: true, //DA LA PROPIEDAD A ID DE QUE ES LA CLAVE PRIMARIA
     },
     name: {
       type: DataTypes.STRING,
