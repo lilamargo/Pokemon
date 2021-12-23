b64f374786346e78f48bf893d8da730cc02d957d
lilamargo <liliana.proy@gmail.com>
2021-12-23 00:27:40 -0600

    Deploy final 6

--- PI-Pokemon-main/client/src/index.js
+++ PI-Pokemon-main/client/src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/index.js";
-import * as serviceWorker from "./serviceWorker";
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
@@ -19,8 +18,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
